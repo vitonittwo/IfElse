b1adfa48b0d6d45be9f8aa732ed6fed834bd0cb1
vitonittwo <vorslvv33@inbox.lv>
2021-01-25 20:22:44 +0200

    First commit IfElse

---
+++ src/ifElse.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+
+public class ifElse {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+
+
+    public static void main(String[] args) {
+
+        int N = scanner.nextInt();
+        // scanner.skip("(\r\n| [\n\r\u2028\u2029\u0085])?");
+        // scanner.close();
+
+        if (( N%2) == 0 ) {
+
+            System.out.println("Weired");
+        }
+        else {
+            if (2 < N && N <= 5) {
+
+                    System.out.println("Not Weired");
+                }
+            else {
+                    if (6 < N && N < 20) {
+                        System.out.println("Weired");
+                    }
+                    else {
+                        if (N > 20) {
+                            System.out.println("Not Weired");
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
+
