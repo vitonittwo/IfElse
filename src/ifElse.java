6ddeb8c92d696fa6c6415642ae606a6ca59c57e4
vitonittwo <vorslvv33@inbox.lv>
2021-01-26 19:20:55 +0200

    bug fix

--- src/ifElse.java
+++ src/ifElse.java
@@ -14,30 +14,17 @@ public class ifElse {
          scanner.skip("(\r\n| [\n\r\u2028\u2029\u0085])?");
          scanner.close();
 
-        if (( N%2) == 0 ) {
+        if (( N%2) == 0 || (6 < N && N < 20) ) {
 
             System.out.println("Weired");
         }
         else {
-            if (2 < N && N <= 5) {
-
-                    System.out.println("Not Weired");
-                }
-            else {
-                    if (6 < N && N < 20) {
-                        System.out.println("Weired");
-                    }
-                    else {
-                        if (N > 20) {
-                            System.out.println("Not Weired");
-                        }
-                    }
+             System.out.println("Not Weired");
+        }
                 }
             }
-        }
 
 
-    }
 
 
 
